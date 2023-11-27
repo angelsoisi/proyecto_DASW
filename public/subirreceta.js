633ed58799da6ba1badc3b18a690b26bf4b7a9e2
Cr4zyD14m0nd137 <77192890+Cr4zyD14m0nd137@users.noreply.github.com>
2023-11-27 03:18:53 -0600

    DOM regresa una lista undefined y varios errores arreglados

--- public/subirreceta.js
+++ public/subirreceta.js
@@ -1,25 +1,43 @@
 "use strict";
-document.getElementById('lista-recetas').addEventListener('submit', function(event) {
-    event.preventDefault();
 
-    var formData = new FormData(this);
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('recipeForm');
+    if(form) {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
 
-    fetch('/subirReceta', {
-        method: 'POST',
-        body: formData
-        // Aquí no estableces el encabezado 'Content-Type' ya que FormData lo hará por ti
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok.');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
-        window.location.href = '/misrecetas.html'; // Redirigir al usuario
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+            var nombre = document.getElementById('nombre').value;
+            var descripcion = document.getElementById('descripcion').value;
+            var preparacion = document.getElementById('preparacion').value;
+            var tiempo = document.getElementById('tiempo').value;
+            var imagen = document.getElementById('imagen').value;
+
+            fetch('/subirReceta', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    nombre: nombre,
+                    descripcion: descripcion,
+                    preparacion: preparacion,
+                    tiempo: tiempo,
+                    imagen: imagen
+                })
+            })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok.');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Success:', data);
+                window.location.href = '/misrecetas.html'; // Redirigir al usuario
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        });
+    }
 });
