4069592f860a668ddd912e0c7b52541e5c01c374
Eduardo Javier García López <eduardo.lalo1999@gmail.com>
2023-12-01 05:11:48 -0600

    Refactor search input and button IDs

--- public/misrecetas.js
+++ public/misrecetas.js
@@ -2,48 +2,69 @@
 
 // Al cargar la página, recuperamos las recetas y las mostramos
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('/obtenerRecetas')
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(recetas => {
-        console.log('Recetas fetched:', recetas); // Log the fetched recipes
-        const listaRecetas = document.getElementById('lista-recetas'); // Asegúrate de que este elemento exista en tu HTML
+  function searchRecipes() {
+    var input, filter, recipesContainer, recipes, title, i, txtValue;
+    input = document.getElementById('searchInput');
+    filter = input.value.toUpperCase();
+    recipesContainer = document.getElementById('lista-recetas');
+    recipes = recipesContainer.getElementsByClassName('recipe');
+  
+    for (i = 0; i < recipes.length; i++) {
+      title = recipes[i].getElementsByClassName('recipe-title')[0];
+      txtValue = title.textContent || title.innerText;
+  
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        recipes[i].style.display = "";
+      } else {
+        recipes[i].style.display = "none";
+      }
+    }
+  }
 
-        // Limpiar listaRecetas antes de agregar nuevos elementos
-        listaRecetas.innerHTML = '';
+  document.getElementById('searchButton').addEventListener('click', searchRecipes);
 
-        // Iterar sobre el array de recetas y crear un elemento para cada una
-        recetas.forEach(receta => {
-            const elementoReceta = document.createElement('div');
-            elementoReceta.innerHTML = `
-                <div class="col-md-4" >
-                  <div class="card">
-                    <a href="/recipe.html?id=${receta._id}">
-                      <img
-                        src="${receta.imagen}"
-                        class="card-img-top"
-                        alt="${receta.nombre}"
-                      />
-                    </a>
-                    <div class="card-body">
-                      <div class="overlay">
-                        <a href="recipe.html?id=${receta._id}" class="animated-text">Descubrir!</a>
-                      </div>
-                      <h5 class="card-title">${receta.nombre}</h5>
-                      <p class="card-text">${receta.descripcion}</p>
+  fetch('/obtenerRecetas')
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Network response was not ok ' + response.statusText);
+      }
+      return response.json();
+  })
+  .then(recetas => {
+      console.log('Recetas fetched:', recetas); // Log the fetched recipes
+      const listaRecetas = document.getElementById('lista-recetas'); // Asegúrate de que este elemento exista en tu HTML
+
+      // Limpiar listaRecetas antes de agregar nuevos elementos
+      listaRecetas.innerHTML = '';
+
+      // Iterar sobre el array de recetas y crear un elemento para cada una
+      recetas.forEach(receta => {
+          const elementoReceta = document.createElement('div');
+          elementoReceta.innerHTML = `
+              <div class="col-md-4" >
+                <div class="card">
+                  <a href="/recipe.html?id=${receta._id}">
+                    <img
+                      src="${receta.imagen}"
+                      class="card-img-top"
+                      alt="${receta.nombre}"
+                    />
+                  </a>
+                  <div class="card-body">
+                    <div class="overlay">
+                      <a href="recipe.html?id=${receta._id}" class="animated-text">Descubrir!</a>
                     </div>
+                    <h5 class="card-title">${receta.nombre}</h5>
+                    <p class="card-text">${receta.descripcion}</p>
                   </div>
                 </div>
-            `;
-            listaRecetas.appendChild(elementoReceta);
-        });
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+              </div>
+          `;
+          listaRecetas.appendChild(elementoReceta);
+      });
+  })
+  .catch(error => {
+      console.error('Error:', error);
+  });
 });
 
